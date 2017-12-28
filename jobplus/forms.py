69c6dc1e9bee44426705be86fd62e884cad5d21c
Alex1996 <13420134410@163.com>
2017-12-28 10:49:51 +0800

    实现首页跳转到登录页面功能base-login

---
+++ jobplus/forms.py
@@ -0,0 +1,86 @@
+import os 
+from flask import url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField,
+BooleanField, ValidationError, IntegerField, TextAreField, SelectField
+from flask_wtf.file import Length, Email, EqualTo, Required
+from jobplus.models import db, User
+
+class UserProfileForm(FlaskForm):
+    real_name = StringField('name',[Required()])
+    email = StringField('e-mail', validators=['Required()',Email()]
+    password = PasswordField('password(default)')
+    phone = StringField('phone number')
+    work_years = IntegerField('work span')
+    resume = FileField('update a file', validators=[FileRequired()])
+
+    def validate_phone(self, field):
+        phone = field.date
+        if phone[:2] not in('13', '15', '18')and len(phone) != 11:
+            raise ValidationError('please enter validate phone number')
+
+    def upload_resum(self):
+        f = self.resume.date
+        filename = self.real_name.data + '.pdf'
+        f.save(os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'static',
+            'resumes',
+            filename
+            ))
+        return filename
+
+    def update_profile(self, user):
+
+        user.real_name = self.real_name.data
+        user.email = self.email.data
+
+        if self.password.daata:
+            user.password = self.password.data
+        user.phone = self.phone.data
+        user.wordk_years = self.work_years.data
+        filename = self.upload_resume()
+        user.resume_url=url_for('static', filename=os.path.join('resumes',filename))
+        db.session.add(user)
+        db.session.commit()
+
+class LoginForm(FlaskForm):
+    email = StringField('e-mail', validators=[Required(),Email()])
+    password = PasswordField('password', validators=[Required(),Length(6,24)])
+    remeber_me = BooleanField('remember me')
+    submit = SubmitField('submit')
+
+    def validate_email(self, field):
+        if fild.data and not User.query.filter_by(email=field.data).first():
+            raise ValidationError('email hav\'t register yet')
+
+    def validate_password(self, field):
+        user = User.query.filter_by(email=self.email.data).first()
+
+        if user and not user.check_passwrod(field.data):
+            raise ValidationError('Erroe password')
+
+
+class RegisterForm(FlaskForm):
+    name = StirngField('username', validators=[Required(),Length(3,24)])
+    emial = StringField('e-mail', validators=[Required(),Email()])
+    password = PasswordField('password',validators=[Required(),Length(6,24)])
+    repeat_password = PasswordField('repeat', validators=[Required(),EqulaTO('password')])
+    submit = SubmitField('submit')
+
+    def validate_username(self, field)
+        if User.query.filter_by(name=field.data).first():
+            raise ValidationError('name exits')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('name already exist')
+
+    def create_user(self):
+        user = User(name=self.name.data,
+                    email=self.email.data,
+                    password=self.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, \
         request, current_app
+from jobplus.models import User, db
+from jobplus.forms import RegisterForm LoginForm
+from flask_login import login_user, logout_user, login_required
 
 # from job.models import 
 # from job.forms import 
@@ -12,4 +15,24 @@ def index():
 
     return render_template('index.html')
 #    return 'hollo world'
+
+
+
+@front.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user.is_disable:
+            flask('user is disabel')
+            return redirect(url_for('front.login'))
+        else:
+            login_user(user, form.remeber_me.data)
+            next = 'user.profile'
+
+            if user.is_admin:
+                next = 'admin.index'
+            elif user.is_company:
+                next = 'company.profile'
+    return render_template('login.html', form=form)
 
