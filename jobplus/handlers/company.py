b0bda1b2eb51a89b1b082d58e50db76457df759f
Alex1996 <13420134410@163.com>
2018-01-11 22:57:05 +0800

    unfished

--- jobplus/handlers/company.py
+++ jobplus/handlers/company.py
@@ -1,5 +1,5 @@
 from flask import Blueprint , render_template, request, current_app, \
-    redirect, url_for, flash
+    redirect, url_for, flash , abort 
 
 from flask_login import login_required, current_user
 from jobplus.forms import CompanyProfileForm
@@ -65,4 +65,68 @@ def company_jobs(company_id):
         abort(404)
     return render_template('company/detail.html', company=company,active='',panel='job')
 
+@company.route('/<int:company_id>/admin')
+@login_required
+def admin_index(company_id):
+    if not current_user.is_admin and not current_user.id == company_id:
+        abort(404)
+
+    page = request.args.get('page', default=1, type=int)
+    pagination = Job.query.filter_by(company_id=company_id).paginate(
+            page=page,
+            per_page=current_app.config['ADMIN_PER_PAGE'],
+            error_out=False
+            )
+    return render_template('company/admin_index.html',
+            company_id=company_id, pagination=pagination) 
+
+@company.route('/<int:company_id>/admin/apply')
+@login_required
+def admin_apply(company_id):
+
+    if not current_user.is_admin and not current_user.id == company_id:
+        abort(404)
+
+    status = request.args.get('status', 'all')
+    page = request.args.get('page', default=1, type=int)
+    q = Delivery.query.filter_by(company_id=company_id)
+
+    if stauts == 'waiting':
+        q = q.filter(Delivery.status == Delivery.STATUS_WAITING)
+    elif status == 'reject':
+        q = q.filter(Delivery.status==Delivery.STATUS_REJECT)
+
+    pagination = q.order_by(Delivery.created_at.desc()).pagintate(
+            page=page,
+            per_page=current_app.config['ADMIN_PER_PAGE'],
+            error_out=False
+            )
+    return render_template('company/admin_apply.html',pagination=pagination,company_id=company_id)
+
+
+@company.route('/<int:company_id>/admin/apply/<int:delivery_id>/reject/')
+@login_required
+def admin_apply_reject(company_id, delivery_id):
+    d = Delivery.query.get_or_404(delivery_id)
+    if current_user.id != company_id:
+        abort(404)
+    d.status = Delivery.STATUS_REJECT
+    db.session.add(d)
+    db.session.commit()
+
+    return redirect(url_for('company.admin_apply',company_id=company_id))
+@company.route('/<int:company_id>/admin/apply/<int:delivery_id>/accept/')
+@login_required
+def admin_apply_accept(company_id, delivery_id):
+    d = Delivery.query.get_or_404(delivery_id)
+    if current_user.id != company_id:
+        abort(404)
+    job = Job.query.get_or_404(job_id)
+    if job.company_id != current_user.id:
+        abort(404)
+
+    db.session.delete(job)
+    de.session.commit()
+    flash('opening upgrade success', 'success')
+    return redirect(url_for('company.admin_index', company_id=current_user.id))
 
